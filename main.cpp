2c6d04a69a24769b3bfb87fbc41dacaaac0c0a26
Vadim-Burns <vadim.burns@protonmail.com>
2022-05-30 20:50:10 +0000

    battles ready

--- main.cpp
+++ main.cpp
@@ -69,21 +69,18 @@ class Ant : public Talkative {
 	std::cout << _queenTalk << std::endl;
   }
 
-  Ant(const int type, const char* queen_talk) {
+  Ant(const int type, const char *queen_talk) {
 	switch (type) {
-	  case WARRIOR_TYPE:
-		_health = 1;
+	  case WARRIOR_TYPE: _health = 1;
 		_defence = 0;
 		_damage = 1;
 		_typeName = WARRIOR;
 		break;
-	  case WORKER_TYPE:
-		_health = 1;
+	  case WORKER_TYPE: _health = 1;
 		_defence = 0;
 		_damage = 0;
 		break;
-	  default:
-		throw std::invalid_argument("auto generate only default worker or warrior");
+	  default: throw std::invalid_argument("auto generate only default worker or warrior");
 	}
 
 	_rankName = DEFAULT_RANK;
@@ -100,15 +97,12 @@ class Ant : public Talkative {
 		_type(type) {
 	switch (type) {
 	  case WORKER_TYPE:
-	  case WORKER_SPECIAL_TYPE:
-		_typeName = WORKER;
+	  case WORKER_SPECIAL_TYPE: _typeName = WORKER;
 		break;
 	  case WARRIOR_TYPE:
-	  case WARRIOR_SPECIAL_TYPE:
-		_typeName = WARRIOR;
+	  case WARRIOR_SPECIAL_TYPE: _typeName = WARRIOR;
 		break;
-	  default:
-		_typeName = SPECIAL;
+	  default: _typeName = SPECIAL;
 	}
   }
 
@@ -157,8 +151,8 @@ class Ant : public Talkative {
 
 class Queen {
  protected:
-  const char* _name;
-  const char* _queenTalk;
+  const char *_name;
+  const char *_queenTalk;
 
   // количество дней высидки
   const int _period_max;
@@ -174,7 +168,7 @@ class Queen {
 
   bool isChildrenReady() {
 	if (!_ready)
-		_ready = _cur_day >= _period_min && (randomN(_cur_day, _period_max) == _cur_day);
+	  _ready = _cur_day >= _period_min && (randomN(_cur_day, _period_max) == _cur_day);
 	return _ready;
   }
 
@@ -204,10 +198,10 @@ class Queen {
 	return _period_max - _cur_day;
   }
 
-  std::vector<Ant>* getChildren() {
+  std::vector<Ant> *getChildren() {
 	if (!isChildrenReady()) return nullptr;
 
-	auto* children = new std::vector<Ant>;
+	auto *children = new std::vector<Ant>;
 
 	int count_ch = randomN(_count_ch_min, _count_ch_max);
 
@@ -216,8 +210,8 @@ class Queen {
 		  *new Ant(
 			  (randomN(0, 1) ? WARRIOR_TYPE : WORKER_TYPE),
 			  _queenTalk
-			  )
-		  );
+		  )
+	  );
 	}
 
 	_cur_day = 0;
@@ -260,10 +254,11 @@ class Colony : public TalkativeColony {
   }
 
   void getChildren() {
-	auto* children = _queen->getChildren();
+	auto *children = _queen->getChildren();
 
 	if (children == nullptr) {
-	  std::cout << "---Потомство: еще не готовы(осталось не больше " << _queen->getDaysBefore() << " дней)" << std::endl;
+	  std::cout << "---Потомство: еще не готовы(осталось не больше " << _queen->getDaysBefore() << " дней)"
+				<< std::endl;
 	  return;
 	}
 
@@ -309,15 +304,15 @@ class Colony : public TalkativeColony {
 	return _specials.size();
   }
 
-  Ant& getWorker(int index) {
+  Ant &getWorker(int index) {
 	return _workers[index];
   }
 
-  Ant& getWarrior(int index) {
+  Ant &getWarrior(int index) {
 	return _warriors[index];
   }
 
-  Ant& getSpecial(int index) {
+  Ant &getSpecial(int index) {
 	return _specials[index];
   }
 
@@ -333,7 +328,7 @@ class Colony : public TalkativeColony {
 	_specials.erase(_specials.begin() + index);
   }
 
-  void killWarriors(const std::vector<int>& indexes) {
+  void killWarriors(const std::vector<int> &indexes) {
 	int cnt = 0;
 
 	for (auto i : indexes) {
@@ -342,17 +337,30 @@ class Colony : public TalkativeColony {
 	}
   }
 
-  void killWorkers(const std::vector<int>& indexes) {
+  void killWorkers(const std::vector<int> &indexes) {
 	int cnt = 0;
 
-	for (auto i: indexes) {
+	for (auto i : indexes) {
 	  killWorker(i - cnt);
 	  cnt++;
 	}
   }
+
+  void killSpecials(const std::vector<int> &indexes) {
+	int cnt = 0;
+
+	for (auto i : indexes) {
+	  killSpecial(i - cnt);
+	  cnt++;
+	}
+  }
+
+  int resSum() {
+	return _kamyshek + _listik + _rosinka + _vetochka;
+  }
 };
 
-class Heap: public TalkativeHeap {
+class Heap : public TalkativeHeap {
  protected:
   short _number;
 
@@ -361,7 +369,6 @@ class Heap: public TalkativeHeap {
   int _vetochka;
   int _listik;
   int _rosinka;
-
 
  public:
   short getNumber() const {
@@ -417,7 +424,7 @@ class HeapStorage {
 
  public:
   void talk() const {
-	for (const auto & _heap : _heaps) {
+	for (const auto &_heap : _heaps) {
 	  _heap.talk();
 	}
   }
@@ -425,19 +432,19 @@ class HeapStorage {
   int getAliveCount() const {
 	int count = 0;
 
-	for (const auto & _heap : _heaps) {
+	for (const auto &_heap : _heaps) {
 	  if (!_heap.isDead()) count++;
 	}
 
 	return count;
   }
 
-  Heap* getRandomAliveHeap() {
+  Heap *getRandomAliveHeap() {
 	if (getAliveCount() == 0) return nullptr;
 
 	int heap_n = randomN(0, _heaps.size() - 1);
 
-	Heap* cur_heap = &_heaps[heap_n];
+	Heap *cur_heap = &_heaps[heap_n];
 	while (cur_heap->isDead()) {
 	  heap_n = (heap_n + 1) % _heaps.size();
 	  cur_heap = &_heaps[heap_n];
@@ -446,7 +453,7 @@ class HeapStorage {
 	return cur_heap;
   }
 
-  void addHeap(Heap& heap) {
+  void addHeap(Heap &heap) {
 	_heaps.push_back(heap);
   }
 };
@@ -460,15 +467,14 @@ class AbstractGameProcessor {
   virtual void talk() const = 0;
 };
 
-
-class GameProcessor: public AbstractGameProcessor{
+class GameProcessor : public AbstractGameProcessor {
  protected:
   int _days;
 
-  HeapStorage* _heap_storage;
+  HeapStorage *_heap_storage;
 
-  Colony* _colony1;
-  Colony* _colony2;
+  Colony *_colony1;
+  Colony *_colony2;
 
   void startBattle() {
 	std::cout << "Обе колонии выбрали одну кучу! Начинается битва" << std::endl;
@@ -495,8 +501,6 @@ class GameProcessor: public AbstractGameProcessor{
 		  wor2.push_back(j);
 		}
 	  }
-
-	  // TODO: specials
 	} else {
 	  int i;
 	  for (i = 0; i < _colony1->getWarriorsSize(); i++) {
@@ -510,13 +514,58 @@ class GameProcessor: public AbstractGameProcessor{
 		  wor1.push_back(j);
 		}
 	  }
+	}
 
-	  // TODO: specials
+	// особые особи, обладая повышенным интеллектом атакуют в первую очередь особых и рабочих, так как войны не имеют
+	// для них значения
+	if (_colony1->getSpecialsSize() > 0 || _colony2->getSpecialsSize() > 0) {
+	  if (_colony2->getSpecialsSize() > 0) {
+		// атака второй колонии
+		if (_colony1->getSpecialsSize() > 0) {
+		  if (_colony1->getSpecial(0).getBeat(_colony2->getSpecial(0).getDamage())) scp1.push_back(0);
+		}
+		else if (_colony1->getWorkersSize() - wor1.size() > 0) {
+			wor1.push_back(_colony1->getWorkersSize() - 1);
+		} else if (_colony1->getWarriorsSize() - war1.size() > 0) {
+		  if (_colony1->getWarrior(_colony1->getWarriorsSize() - 1).getBeat(_colony2->getSpecial(0).getDamage()))
+			war1.push_back(_colony1->getWarriorsSize() - 1);
+		}
+	  }
+
+	  // атака первой колонии
+	  if (_colony1->getSpecialsSize() > 0) {
+		int attcks = 0;
+		if (_colony2->getSpecialsSize() > 0) {
+		  if (_colony2->getSpecial(0).getBeat(_colony1->getSpecial(0).getDamage())) scp2.push_back(0);
+		  attcks++;
+		}
+		if (attcks < 2 && _colony2->getWorkersSize() - wor2.size() > 0) {
+		  attcks++;
+		  wor2.push_back(_colony2->getWorkersSize() - 1);
+		}
+		if (attcks < 2 && _colony2->getWorkersSize() - wor2.size() > 0) {
+		  attcks++;
+		  wor2.push_back(_colony2->getWorkersSize() - 2);
+		}
+
+		if (attcks < 2 && _colony2->getWarriorsSize() - war2.size() > 0) {
+		  attcks++;
+		  if (_colony2->getWarrior(_colony2->getWarriorsSize() - 1).getBeat(_colony1->getSpecial(0).getDamage()))
+			war2.push_back(_colony2->getWarriorsSize() - 1);
+		}
+		if (attcks < 2 && _colony2->getWarriorsSize() - war2.size() > 0) {
+		  attcks++;
+		  if (_colony2->getWarrior(_colony2->getWarriorsSize() - 2).getBeat(_colony1->getSpecial(0).getDamage()))
+			war2.push_back(_colony2->getWarriorsSize() - 2);
+		}
+	  }
 	}
 
 	std::cout << "Битва окончена." << std::endl;
-	std::cout << "Колония \'" << _colony1->getName() << "\' потеряла:р=" << wor1.size() << ", в=" << war1.size() << ", о=" << scp1.size() << std::endl;
-	std::cout << "Колония \'" << _colony2->getName() << "\' потеряла:р=" << wor2.size() << ", в=" << war2.size() << ", о=" << scp2.size() << std::endl;
+	std::cout << "Колония \'" << _colony1->getName() << "\' потеряла:р=" << wor1.size() << ", в=" << war1.size()
+			  << ", о=" << scp1.size() << std::endl;
+	std::cout << "Колония \'" << _colony2->getName() << "\' потеряла:р=" << wor2.size() << ", в=" << war2.size()
+			  << ", о=" << scp2.size() << std::endl;
 	std::cout << "-------------------------------------------" << std::endl;
 
 	_colony1->killWarriors(war1);
@@ -524,11 +573,14 @@ class GameProcessor: public AbstractGameProcessor{
 
 	_colony1->killWorkers(wor1);
 	_colony2->killWorkers(wor2);
+
+	_colony1->killSpecials(scp1);
+	_colony2->killSpecials(scp2);
   }
 
   void startWalk() {
 	std::cout << std::endl;
-	Heap* heap1 = _heap_storage->getRandomAliveHeap();
+	Heap *heap1 = _heap_storage->getRandomAliveHeap();
 
 	if (heap1 == nullptr) {
 	  std::cout << "Походы более невозможны по причине истощения куч";
@@ -536,12 +588,14 @@ class GameProcessor: public AbstractGameProcessor{
 	}
 
 	std::cout << "Колония \'" << _colony1->getName() << "\' начала поход на кучу " << heap1->getNumber();
-	std::cout << ": р=" << _colony1->getWorkersSize() << ", в=" << _colony1->getWarriorsSize() << ", о=" << _colony1->getSpecialsSize();
+	std::cout << ": р=" << _colony1->getWorkersSize() << ", в=" << _colony1->getWarriorsSize() << ", о="
+			  << _colony1->getSpecialsSize();
 	std::cout << std::endl;
 
-	Heap* heap2 = _heap_storage->getRandomAliveHeap();
+	Heap *heap2 = _heap_storage->getRandomAliveHeap();
 	std::cout << "Колония \'" << _colony2->getName() << "\' начала поход на кучу " << heap2->getNumber();
-	std::cout << ": р=" << _colony2->getWorkersSize() << ", в=" << _colony2->getWarriorsSize() << ", о=" << _colony2->getSpecialsSize();
+	std::cout << ": р=" << _colony2->getWorkersSize() << ", в=" << _colony2->getWarriorsSize() << ", о="
+			  << _colony2->getSpecialsSize();
 	std::cout << std::endl;
 
 	if (heap1 == heap2) {
@@ -597,51 +651,77 @@ class GameProcessor: public AbstractGameProcessor{
 	_heap_storage->talk();
 	std::cout << "-------------------------------------------" << std::endl;
   }
+
+  const char* getWinnerName() {
+	return _colony1->resSum() > _colony2->resSum() ? _colony1->getName() : _colony2->getName();
+  }
 };
 
-std::vector<Ant>* generateWorkers(int size, const char* queen_talk) {
-  auto* workers = new std::vector<Ant>;
+std::vector<Ant> *generateWorkers(int size, const char *queen_talk) {
+  auto *workers = new std::vector<Ant>;
 
   for (int i = 0; i < size; i++) {
 	workers->push_back(
 		*new Ant(
 			WORKER_TYPE,
 			queen_talk
-			)
-		);
+		)
+	);
   }
 
   return workers;
 }
 
-std::vector<Ant>* generateWarriors(int size, const char* queen_talk) {
-  auto* warriors = new std::vector<Ant>;
+std::vector<Ant> *generateWarriors(int size, const char *queen_talk) {
+  auto *warriors = new std::vector<Ant>;
 
   for (int i = 0; i < size; i++) {
 	warriors->push_back(
 		*new Ant(
-				WARRIOR_TYPE,
-				queen_talk
-			)
-		);
+			WARRIOR_TYPE,
+			queen_talk
+		)
+	);
   }
 
   return warriors;
 }
 
-Colony* generateOrangeColony() {
-  auto* workers = generateWorkers(18, "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев");
-  auto* warriors = generateWarriors(9, "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев");
-  auto* specials = new std::vector<Ant>;
+Colony *generateOrangeColony() {
+  auto *workers = generateWorkers(18, "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев");
+  auto *warriors = generateWarriors(8, "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев");
+  auto *specials = new std::vector<Ant>;
 
-  Queen* queen = new Queen(
+  warriors->push_back(
+	  *new Ant(
+		  8,
+		  4,
+		  5,
+		  "элитный легенда",
+		  "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев",
+		  WARRIOR_TYPE
+		  )
+	  );
+
+  specials->push_back(
+	  *new Ant(
+			  21,
+			  9,
+			  8,
+			  "ленивый неуязвимый агрессивный крепкий - Шмель",
+			  "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев",
+			  SPECIAL_TYPE
+		  )
+	  );
+
+  Queen *queen = new Queen(
 	  "Беатрикс",
 	  "Королева <Беатрикс>, цикл роста личинок 2-4 дней, может создать 1-5 королев",
 	  4,
 	  2,
 	  1,
 	  5
-	  );
+  );
 
   return new Colony(
 	  "рыжие",
@@ -649,22 +729,66 @@ Colony* generateOrangeColony() {
 	  *workers,
 	  *warriors,
 	  *specials
-	  );
+  );
 }
 
-Colony* generateBlackColony() {
-  auto* workers = generateWorkers(18, "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев");
-  auto* warriors = generateWarriors(8, "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев");
-  auto* specials = new std::vector<Ant>;
+Colony *generateBlackColony() {
+  auto *workers = generateWorkers(18, "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев");
+  auto *warriors = generateWarriors(5, "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев");
+  auto *specials = new std::vector<Ant>;
 
-  Queen* queen = new Queen(
+  warriors->push_back(
+	  *new Ant(
+		  2,
+		  1,
+		  2,
+		  "старший",
+		  "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев",
+		  WARRIOR_TYPE
+	  )
+  );
+
+  warriors->push_back(
+	  *new Ant(
+		  8,
+		  4,
+		  5,
+		  "элитный",
+		  "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев",
+		  WARRIOR_TYPE
+	  )
+  );
+
+  warriors->push_back(
+	  *new Ant(
+		  10,
+		  6,
+		  4,
+		  "легендарный мифический",
+		  "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев",
+		  WARRIOR_TYPE
+	  )
+  );
+
+  specials->push_back(
+	  *new Ant(
+		  29,
+		  6,
+		  28,
+		  "трудолюбивый неуязвимый агрессивный сосредоточенный невезучий - Толстоножка",
+		  "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев",
+		  SPECIAL_TYPE
+	  )
+  );
+
+  Queen *queen = new Queen(
 	  "Жозефина",
 	  "Королева <Жозефина>, цикл роста личинок 3-3 дней, может создать 1-5 королев",
 	  3,
 	  3,
 	  1,
 	  5
-	  );
+  );
 
   return new Colony(
 	  "черные",
@@ -672,51 +796,51 @@ Colony* generateBlackColony() {
 	  *workers,
 	  *warriors,
 	  *specials
-	  );
+  );
 }
 
-HeapStorage* generateHeapStorage() {
-  auto* heap1 = new Heap(
+HeapStorage *generateHeapStorage() {
+  auto *heap1 = new Heap(
 	  1,
 	  0,
 	  13,
 	  49,
 	  0
-	  );
+  );
 
-  auto* heap2 = new Heap(
+  auto *heap2 = new Heap(
 	  2,
 	  43,
 	  40,
 	  18,
-	26
-	  );
+	  26
+  );
 
-  auto* heap3 = new Heap(
+  auto *heap3 = new Heap(
 	  3,
 	  21,
 	  45,
 	  36,
 	  19
-	  );
+  );
 
-  auto* heap4 = new Heap(
+  auto *heap4 = new Heap(
 	  4,
 	  13,
 	  23,
 	  30,
 	  10
-	  );
+  );
 
-  auto* heap5 = new Heap(
+  auto *heap5 = new Heap(
 	  5,
 	  38,
 	  25,
 	  0,
 	  43
-	  );
+  );
 
-  auto* hs = new HeapStorage();
+  auto *hs = new HeapStorage();
   hs->addHeap(*heap1);
   hs->addHeap(*heap2);
   hs->addHeap(*heap3);
@@ -726,24 +850,27 @@ HeapStorage* generateHeapStorage() {
   return hs;
 }
 
-
 int main() {
   std::cout << "Человечество уничтожено. Последний оплод цивилизации - муравьи, всего 2 колонии." << std::endl;
   std::cout << "До засухи осталось всего " << DAYS_UNTIL << " дней" << std::endl;
   std::cout << "Выживет только сильнейшая колония, приятного просмотра голодных муравьиных игр" << std::endl;
 
-  auto* gp = new GameProcessor(
-	  	DAYS_UNTIL,
-		generateHeapStorage(),
-		generateOrangeColony(),
-		generateBlackColony()
-	  );
+  auto *gp = new GameProcessor(
+	  DAYS_UNTIL,
+	  generateHeapStorage(),
+	  generateOrangeColony(),
+	  generateBlackColony()
+  );
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 14; i++) {
 	gp->talk();
 	gp->nextDay();
   }
 
+  std::cout << "----------------------------------------------------------------" << std::endl;
+  std::cout << "Началась засуха.\nВыжила колония \'" << gp->getWinnerName() << "\'.\nИгра окончена\n";
+  std::cout << "----------------------------------------------------------------" << std::endl;
+  std::cout << "----------------------------------------------------------------" << std::endl;
 
   return EXIT_SUCCESS;
 }
